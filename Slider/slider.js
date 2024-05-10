a6fa8fe93373eb45715b0f24396cde5a4889ad2d
Simge Kaplan <133397130+simgekaplann@users.noreply.github.com>
2024-05-10 17:03:31 +0300

    Add files via upload

---
+++ Slider/slider.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $("#slider").bxSlider({
+        randomStart: true, // 2. Random başlangıç
+        moveSlides: 1, // 3. Her seferinde bir slayt kaydırma
+        minSlides: 1, // 4. Yalnızca bir resmin görüntülenmesi
+        maxSlides: 1,
+        auto: true, // 5. Otomatik geçişler
+        pause: 3000, // 5. 3 saniye bekleme
+		captions: true,
+		pagerCustom: '#pager',
+		pagerType: "short"
+});
+});
