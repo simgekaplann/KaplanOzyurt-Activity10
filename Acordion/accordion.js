1b9a5c95c5070ea7b146d0db947890760e81a8e5
Simge Kaplan <133397130+simgekaplann@users.noreply.github.com>
2024-05-10 17:25:18 +0300

    Delete Acordion directory

--- Acordion/accordion.js
+++
@@ -1,7 +0,0 @@
-  $(document).ready(function() {
-            $("#tabs").accordion({
-                collapsible: true,
-                active: false, 
-                heightStyle: "content" 
-            });
-        });
